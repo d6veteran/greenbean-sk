c660e6a5052688c40ed3a86f9228cd9fe0c1495a
Will Merydith <will.merydith@gmail.com>
2010-12-09 22:48:01 -0600

    Fixed indentation

--- models.py
+++ models.py
@@ -3,12 +3,12 @@
 from google.appengine.ext import db
 
 class User(db.Model):
-    name = db.StringProperty(required=True)
-    profile_url = db.StringProperty(required=True)
-    fb_access_token = db.StringProperty(required=True)
-    fb_id = db.StringProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    updated = db.DateTimeProperty(auto_now=True)
+	name = db.StringProperty(required=True)
+	profile_url = db.StringProperty(required=True)
+	fb_access_token = db.StringProperty(required=True)
+	fb_id = db.StringProperty(required=True)
+	created = db.DateTimeProperty(auto_now_add=True)
+	updated = db.DateTimeProperty(auto_now=True)
 
 class Brag(db.Model):
 	text = db.StringProperty(required=True)
