344c09cbaaafcff076a71b640330d96ac383c644
Samuel Harper <sampyxis@gmail.com>
2010-12-10 18:50:37 -0800

    Pushing up a new set - this is set for my local app engine account

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
+## Moving all models here for now
 # Placeholder of new datastore models.
-
-#Took out the import facebook - this is in the main file
 
 from google.appengine.ext import db
 
@@ -11,6 +10,12 @@ def check_length(string):
     raise db.BadValueError('Status cannot be more than 140 characters')
   return string
 
+# Location provides place context for Brags.
+class Location(db.Model):
+   city = db.StringProperty(required=True)
+   state = db.StringProperty(required=True)
+   country = db.StringProperty(required=True)
+    
 # User needs to accomodate both FB, Twitter and Google.  Not sure if this is 
 # possible.
 class User(db.Model):
@@ -21,8 +26,12 @@ class User(db.Model):
   created = db.DateTimeProperty(auto_now_add=True)
   updated = db.DateTimeProperty(auto_now=True)
   # TODO: provide a common way to define places for Users of FB, Twitter . . .
-  current_location = db.ReferenceProperty(Location, required=True)
+  current_location = db.ReferenceProperty(Location, required=False)
 
+# Category provides taxonomy for Brags.
+class Category(db.Model):
+  name = db.StringProperty(required=True)
+  
 # Brag is a status message posted by a User that is bragging about an 
 # accomplishment under one or more Categories.  Brags are limited to 140
 # characters.  Brags are associated with a specific Location.  Brags can be
@@ -31,30 +40,20 @@ class User(db.Model):
 class Brag(db.Model):
   message = db.StringProperty(required=True, validator=check_length)
   create_date = db.DateTimeProperty(auto_now_add=True)
-  category = db.ReferenceProperty(Category, required=True)
+  category = db.StringListProperty(db.StringProperty) #(db.Key)
   user = db.ReferenceProperty(User, required=True)
-  location = db.ReferenceProperty(Location, required=True)
+  location = db.ReferenceProperty(Location, required=False)
   origin = db.StringProperty(required=True)
 
 # Bean is a vote on a specific Brag.
 class Bean(db.Model):
-  brag = db.ReferenceProperty(Brag, require=True)	
+  brag = db.ReferenceProperty(Brag, required=True)	
   user = db.ReferenceProperty(User, required=True)
   created = db.DateTimeProperty(auto_now_add=True)
 
-# Category provides taxonomy for Brags.
-  class Category(db.Model):
-    name = db.ReferenceProperty(required=True)
-
-# Location provides place context for Brags.
-  class Location(db.Model):
-    city = db.StringProperty(required=True)
-    state = db.StringProperty(required=True)
-    country = db.StringProperty(required=True)
-
 # These are the total Beans awarded to each Brag.
 class BragBeans(db.Model):
-  brag = db.ReferenceProperty(Brag, require=True)	
+  brag = db.ReferenceProperty(Brag, required=True)	
   bean_count = db.IntegerProperty(required=True)
   updated = db.DateTimeProperty(auto_now=True)
 
@@ -78,3 +77,5 @@ class LocationBeans(db.Model):
   bean_count = db.IntegerProperty(required=True)
   updated = db.DateTimeProperty(auto_now=True)
 
+
+##
